ad5a1d22121710835d02632e87d066e69b0e92e1
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-30 07:36:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   Sam: 'Lay Me Down',
 }
 
+function updateObjectWithKeyAndValue(playlist, artistName, songTitle) {
+  var newObj = Object.assign({[artistName]: songTitle}, playlist);
+  return newObj;
+}
+
